13bc44125db61937b516694445804d6c49c28a0d
DanPearce <daniel_pearce@outlook.com>
2023-02-09 21:16:50 +0000

    Add PostDetail view, get function to serialize data

--- posts/urls.py
+++ posts/urls.py
@@ -9,4 +9,5 @@ from django.urls import path
 
 urlpatterns = [
     path('posts/', views.PostList.as_view()),
+    path('posts/<int:pk>', views.PostDetail.as_view()),
 ]
--- posts/views.py
+++ posts/views.py
@@ -4,18 +4,20 @@ Views for posts
 # Imports Internal
 from .models import Post
 from .serializers import PostSerializer
+from ci_pp5_nexus_drf.permissions import IsOwnerOrReadOnly
 # -----------------------------------------------------------------------
 # Third Party
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status, permissions
+from django.http import Http404
 
 
 class PostList(APIView):
     """
     Class based view for Post List
     -   Lists all posts
-    -   Allows a post to be created
+    -   Allows a post to be created if authenticated
     """
     serializer_class = PostSerializer
     permission_classes = [
@@ -45,3 +47,34 @@ class PostList(APIView):
         return Response(
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
         )
+
+
+class PostDetail(APIView):
+    """
+    Class based view for Post Detail
+    -   Allows us to see the full post detail
+    """
+    serializer_class = PostSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_object(self, pk):
+        """
+        Function to get post object by it id (Primary Key)
+        """
+        try:
+            post = Post.objects.get(pk=pk)
+            self.check_object_permissions(self.request, post)
+            return post
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        """
+        Function to return the serialized post data
+        """
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post,
+            context={'request': request}
+        )
+        return Response(serializer.data)
