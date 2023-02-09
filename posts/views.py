7759ed3df2e16a97147215dc1176db7c5cb6307b
DanPearce <daniel_pearce@outlook.com>
2023-02-09 21:33:03 +0000

    Add put and delete functions to PostDetail view

--- posts/views.py
+++ posts/views.py
@@ -78,3 +78,26 @@ class PostDetail(APIView):
             context={'request': request}
         )
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        """
+        Function to update post by its id
+        """
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post,
+            data=request.data,
+            context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        """
+        Function to delete post by id
+        """
+        post = self.get_object(pk)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
