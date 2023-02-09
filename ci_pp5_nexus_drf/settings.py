970d14d329374c7594ff6ee321cab94bd97a2710
DanPearce <daniel_pearce@outlook.com>
2023-02-09 17:00:41 +0000

    Add put function to ProfileDetail, allowing ability to edit profile - fixed issue with cloudinary placeholder images

--- ci_pp5_nexus_drf/settings.py
+++ ci_pp5_nexus_drf/settings.py
@@ -20,7 +20,7 @@ CLOUDINARY_STORAGE = {
     'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
 }
 MEDIA_URL = '/media/'
-DEFUALT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
--- profiles/views.py
+++ profiles/views.py
@@ -9,6 +9,7 @@ from .serializers import ProfileSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import Http404
+from rest_framework import status
 
 
 class ProfileList(APIView):
@@ -27,6 +28,7 @@ class ProfileDetail(APIView):
     Class based view for Profile Detail
     -   Lists a user's profile
     """
+    serializer_class = ProfileSerializer
 
     def get_object(self, pk):
         """
@@ -45,3 +47,14 @@ class ProfileDetail(APIView):
         profile = self.get_object(pk)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        """
+        Function to update a profile by its id
+        """
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
