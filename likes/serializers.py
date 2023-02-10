09cd30a7708956da9f7579b4bb7cf8f00ec7a4a4
DanPearce <daniel_pearce@outlook.com>
2023-02-10 14:23:19 +0000

    Add serializers for likes app

---
+++ likes/serializers.py
@@ -0,0 +1,33 @@
+"""
+Serializers for likes
+"""
+# Imports
+from .models import Like
+# -----------------------------------------------------------------------
+# Third Party
+from django.db import IntegrityError
+from rest_framework import serializers
+
+
+class LikeSerializers(serializers.ModelSerializer):
+    """
+    Class based serializer for Like data
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Like
+        fields = [
+            'id', 'owner', 'post', 'created_on'
+        ]
+
+    def create(self, validated_data):
+        """
+        Function to handle duplication of likes.
+        """
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'Possible duplicate like'
+            })
