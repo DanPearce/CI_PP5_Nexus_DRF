6f0b1a3057da00ad204e1a6bd528279a2b2eefc2
DanPearce <daniel_pearce@outlook.com>
2023-02-10 23:25:33 +0000

    Add default JSON view while not in DEV mode, added naturaltime formats to settings and comments serializers

--- comments/serializers.py
+++ comments/serializers.py
@@ -6,6 +6,7 @@ from .models import Comment
 # -----------------------------------------------------------------------
 # Third Party
 from rest_framework import serializers
+from django.contrib.humanize.templatetags.humanize import naturaltime
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -16,6 +17,8 @@ class CommentSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    created_on = serializers.SerializerMethodField()
+    updated_on = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         """
@@ -23,6 +26,18 @@ class CommentSerializer(serializers.ModelSerializer):
         """
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_created_on(self, obj):
+        """
+        Function to get the humanized time for created_on
+        """
+        return naturaltime(obj.created_on)
+
+    def get_updated_on(self, obj):
+        """
+        Function to get the humanized time for updated_on
+        """
+        return naturaltime(obj.updated_on)
 
     class Meta:
         model = Comment
