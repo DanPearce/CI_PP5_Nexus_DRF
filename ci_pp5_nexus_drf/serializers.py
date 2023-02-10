7aee0accb3616753ee94e0c6ffdd077e4446ae2e
DanPearce <daniel_pearce@outlook.com>
2023-02-10 19:19:02 +0000

    Add JSON Web Tokens and added User serializers

---
+++ ci_pp5_nexus_drf/serializers.py
@@ -0,0 +1,22 @@
+"""
+Serializers for nexus_drf
+"""
+# Imports
+# -----------------------------------------------------------------------
+# Third Party
+from rest_framework import serializers
+from dj_rest_auth.serializers import UserDetailsSerializer
+from rest_framework import serializers
+
+
+class CurrentUserSerializer(UserDetailsSerializer):
+    """
+    Class based serializer for User data
+    """
+    profile_id = serializers.ReadOnlyField(source='profile.id')
+    profile_image = serializers.ReadOnlyField(source='profile.image.url')
+
+    class Meta(UserDetailsSerializer.Meta):
+        fields = UserDetailsSerializer.Meta.fields + (
+            'profile_id', 'profile_image'
+        )
