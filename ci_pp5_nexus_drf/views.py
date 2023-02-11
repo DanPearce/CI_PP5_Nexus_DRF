1d94076943a4f286b66566073cade22595ab8bcd
DanPearce <daniel_pearce@outlook.com>
2023-02-11 00:19:36 +0000

    Add logout_route view to handle dj-rest-auth bug, added to urls

--- ci_pp5_nexus_drf/views.py
+++ ci_pp5_nexus_drf/views.py
@@ -6,6 +6,10 @@ Views for nexus_drf
 # Third Party
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .settings import (
+    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
+    JWT_AUTH_SECURE
+)
 
 
 @api_view()
@@ -13,3 +17,27 @@ def root_route(request):
     return Response({
         'message': 'Welcome to the Nexus Django REST Framework API.'
     })
+
+
+@api_view(['POST'])
+def logout_route(request):
+    response = Response()
+    response.set_cookie(
+        key=JWT_AUTH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+        )
+    response.set_cookie(
+        key=JWT_AUTH_REFRESH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    return response
