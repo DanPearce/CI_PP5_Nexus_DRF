6ad77e5ce856de80ce05be14b96b456e1f059663
DanPearce <daniel_pearce@outlook.com>
2023-02-17 15:37:08 +0000

    JWT setting typo

--- ci_pp5_nexus_drf/settings.py
+++ ci_pp5_nexus_drf/settings.py
@@ -46,7 +46,7 @@ REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
-JWT_AUTH_SAMESITE = None
+JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'ci_pp5_nexus_drf.serializers.CurrentUserSerializer'
